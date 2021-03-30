85245182ce40de8c14b116744a60345dee4fb381
vinhvvdvGithub <vinhvvdv@gmail.com>
2021-03-30 17:32:11 +0700

    setting router folder

--- Day7/Routes/user.route.js
+++ Day7/Routes/user.route.js
@@ -1,46 +1,28 @@
-var express = require("express");
-var low = require('lowdb');
-var FileSync = require('lowdb/adapters/FileSync');
+var express = require('express');
+
 var shortID = require('shortid');
-var adapter = new FileSync('db.json');
-db = low(adapter);
-
-
-
 var app = express();
 
-var port = 3000;
 
-// Set some defaults (required if your JSON file is empty)
-db.defaults({ users: [] })
-    .write();
-
-
-app.set('view engine', 'pug');
-
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
-
-
-app.get('/users', function (req, res) {
+var db = require('../db');
+var route = express.Router();
+route.get('/', function (req, res) {
     res.render('users/user', {
         users: db.get('users').value()
     });
 });
 
-app.get('/users/create', function (req, res) {
+route.get('/create', function (req, res) {
     res.render('users/create');
 });
-
-app.post('/users/create', function (req, res) {
+route.post('/create', function (req, res) {
     //   console.log(req.body);
     req.body.id = shortID.generate();
     db.get('users').push(req.body).write();
     res.redirect('/users');
 });
 
-app.get('/users/:id', function (req, res) {
+route.get('/:id', function (req, res) {
     var id = req.params.id;
     //console.log(id);
     var user = db.get('users').find({ id: id }).value();
@@ -52,7 +34,7 @@ app.get('/users/:id', function (req, res) {
 });
 
 //search method
-app.get('/users/search', function (req, res) {
+route.get('/search', function (req, res) {
     var q = req.query.q;
     // console.log(q);
     var users = db.get('users').value();
@@ -66,7 +48,4 @@ app.get('/users/search', function (req, res) {
     });
 });
 
-
-app.listen(port, function demo() {
-
-});
+module.exports = route;
