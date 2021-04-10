9b8f17094ff85143f67961e04e333ea7b0d576dd
vinhvvdvGithub <vinhvvdv@gmail.com>
2021-04-10 19:31:50 +0700

    no commit

--- Day14/Controller/product.controller.js
+++ Day14/Controller/product.controller.js
@@ -1,6 +1,15 @@
 var db = require('../db');
 
 module.exports.index = function (req, res) {
-    res.render("Products/product");
-    
+    var page = parseInt(req.query.page) || 1;
+    var perPage = 3
+    var start = (page - 1) * perPage;
+    var end = page * perPage;
+    var drop = (page - 1) * perPage;
+    res.render("Products/product", {
+        //products: db.get('products').value().slice(start, end)
+        products: db.get('products').drop(drop).take(perPage).value()
+    });
+
+
 };
